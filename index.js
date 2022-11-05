e007bbb956988537d976906df6eeebe70c3ec0f7
tejas-faldu <115584181+tejas-faldu@users.noreply.github.com>
2022-11-05 23:35:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const mysql = require('mysql')
+var bodyParser = require('body-parser')
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//current time data
+
+app.get('/', (req, res) => {
+  res.send('helloo');
+});
+
+
+
+app.listen(3333, () => console.log("server start"));
